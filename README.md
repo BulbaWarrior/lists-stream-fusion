d73dadf08ee7be3a1e5458136e7d04a84b9530fd
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2022-02-22 12:13:51 +0300

    Add memory profiling instructions to README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # stream-fusion
+
+## How to profile
+
+```sh
+stack build && stack exec -- stream-fusion-exe +RTS -h && hp2ps -i+ -M -m200 -c stream-fusion-exe.hp && open stream-fusion-exe.ps
+```
--- app/Main.hs
+++ app/Main.hs
@@ -15,8 +15,8 @@ map_s :: (a -> b) -> Stream a -> Stream b
 map_s f (Stream next0 s0) = Stream next s0
   where
     next s = case next0 s of
-      Done -> Done
-      Skip s' -> Skip s'
+      Done       -> Done
+      Skip s'    -> Skip s'
       Yield x s' -> Yield (f x) s'
 
 filter_s :: (a -> Bool) -> Stream a -> Stream a
@@ -32,13 +32,13 @@ append_s :: Stream a -> Stream a -> Stream a
 append_s (Stream next0 s0) (Stream next1 s1) = Stream next (Left s0)
   where
     next (Left s) = case next0 s of
-      Done -> Skip (Right s1)
-      Skip s' -> Skip (Left s')
+      Done       -> Skip (Right s1)
+      Skip s'    -> Skip (Left s')
       Yield a s' -> Yield a (Left s')
 
     next (Right s) = case next1 s of
-      Done -> Done
-      Skip s' -> Skip (Right s')
+      Done       -> Done
+      Skip s'    -> Skip (Right s')
       Yield a s' -> Yield a (Right s')
 
 zip_s :: Stream a -> Stream b -> Stream (a, b)
@@ -46,13 +46,13 @@ zip_s (Stream nextA sA0) (Stream nextB sB0) = Stream next (sA0, sB0, Nothing)
   where
     next (sA, sB, Nothing) =
       case nextA sA of
-        Done -> Done
-        Skip sA' -> Skip (sA', sB, Nothing)
+        Done        -> Done
+        Skip sA'    -> Skip (sA', sB, Nothing)
         Yield a sA' -> Skip (sA', sB, Just a)
     next (sA, sB, Just a) =
       case nextB sB of
-        Done -> Done
-        Skip sB' -> Skip (sA, sB', Just a)
+        Done        -> Done
+        Skip sB'    -> Skip (sA, sB', Just a)
         Yield b sB' -> Yield (a, b) (sA, sB', Nothing)
 
 
@@ -63,21 +63,21 @@ concatMap_s :: (a -> Stream b) -> Stream a -> Stream b
 concatMap_s f (Stream next0 s0) = Stream next (s0, Nothing)
   where
     next (s, Nothing) = case next0 s of
-      Done -> Done
-      Skip s' -> Skip (s', Nothing)
+      Done       -> Done
+      Skip s'    -> Skip (s', Nothing)
       Yield x s' -> Skip (s', Just (f x))
 
     next (s, Just (Stream next1 s1)) = case next1 s1 of
-      Done -> Skip (s, Nothing)
-      Skip s1' -> Skip (s, Just (Stream next1 s1'))
+      Done        -> Skip (s, Nothing)
+      Skip s1'    -> Skip (s, Just (Stream next1 s1'))
       Yield x s1' -> Yield x (s, Just (Stream next1 s1'))
 
 foldl_s :: (b -> a -> b) -> b -> Stream a -> b
 foldl_s f z (Stream next s0) = go z s0
   where
     go z s = case next s of
-      Done -> z
-      Skip s' -> go z s'
+      Done       -> z
+      Skip s'    -> go z s'
       Yield x s' -> go (f z x) s'
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
@@ -96,16 +96,16 @@ example xs ys = foldl_s (+) 0 (append_s (stream xs) (stream ys))
 stream :: [a] -> Stream a
 stream xs0 = Stream next xs0
   where
-    next [] = Done
+    next []     = Done
     next (x:xs) = Yield x xs
 
 unstream :: Stream a -> [a]
 unstream (Stream next s0) = unfold s0
   where
     unfold s = case next s of
-      Done -> []
-      Skip s' -> unfold s'
+      Done       -> []
+      Skip s'    -> unfold s'
       Yield x s' -> x:(unfold s')
 
 main :: IO ()
-main = print $ example [1..5] [1..10]-- unstream (stream (unstream (stream [1, 2, 3])))
+main = print $ example [1..10^7] [1..10^7]-- unstream (stream (unstream (stream [1, 2, 3])))
